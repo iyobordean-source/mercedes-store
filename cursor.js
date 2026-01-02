c0e13a9660f8f21c2b6d213bc9aba8666d7df82d
Iyobor Dean Aisosa <iyobordean@gmail.com>
2026-01-02 22:02:59 +0000

    second commit

--- cursor.js
+++
@@ -1,7 +0,0 @@
-const cursor = document.createElement("div");
-cursor.className = "lux-cursor";
-document.body.appendChild(cursor);
-
-window.addEventListener("mousemove", e => {
-  cursor.style.transform = `translate(${e.clientX}px, ${e.clientY}px)`;
-});
--- page-transition.js
+++
@@ -1,9 +0,0 @@
-document.querySelectorAll("a").forEach(link => {
-  link.addEventListener("click", e => {
-    if (link.href) {
-      e.preventDefault();
-      document.body.style.opacity = 0;
-      setTimeout(() => location.href = link.href, 300);
-    }
-  });
-});
